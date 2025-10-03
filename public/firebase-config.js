fd88970a01befc135f6506381051424a0736bc33
Arltriaa <pokemonrgby@gmail.com>
2025-10-03 10:35:42 +0900

    Delete public/firebase-config.js

--- public/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// /public/firebase-config.js
-window.__FBCONFIG__ = {
-  apiKey: "AIzaSyClPafyxxBjObZ3WZ-QI6xYmPcHuoKMlQA",
-  authDomain: "peaceful-doodad-471301-s2.firebaseapp.com",
-  projectId: "peaceful-doodad-471301-s2",
-  storageBucket: "peaceful-doodad-471301-s2.firebasestorage.app",
-  messagingSenderId: "254091615936",
-  appId: "1:254091615936:web:1b137a251533cd5a8a9725"
-};
